e210adf6a0d0768d5a608ceaf39ce3f0b9da8cea
GustavoDuarte-ufc <gustavoduarte2804@gmail.com>
2023-05-25 09:59:06 -0300

    Resolvido

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-"""Module with functions."""
-
-
 def largest_sum(numbers: list[int]) -> tuple[int, int]:
-    """Encontra e retorna os dois números que somados dão o maior valor."""
     numbers.sort()
     a = len(numbers)
     if a < 2:
-        print(None)
+        return None
     else:
         primeiro = numbers[a-1]  # o primeiro número da soma
         segundo = numbers[a-2]  # o segundo número da soma
-        print(f"({segundo}, {primeiro})")
-    return numbers
-lista = []
-largest_sum(lista)
+        resultado_esperado = (segundo, primeiro)
+        return resultado_esperado
